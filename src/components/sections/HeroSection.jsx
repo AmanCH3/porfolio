103d35adf1561c5bf98f770594cde81ca808d0b6
amanch3 <armanachaudhary53@gmail.com>
2026-04-02 10:19:53 +0545

    feat : change profile

--- src/components/sections/HeroSection.jsx
+++ src/components/sections/HeroSection.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { motion } from 'framer-motion';
 import { FaArrowRight, FaLinkedin, FaGithub, FaChartBar, FaCheckCircle, FaBriefcase } from 'react-icons/fa';
 import Magnetic from '../common/Magnetic';
-import profileImg from '../../assets/profile.png';
+import profileImg from '../../assets/profile.jpg';
 import './Sections.css';
 
 const stats = [
